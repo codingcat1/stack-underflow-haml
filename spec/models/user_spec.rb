76ae0edc79396bf4cbe15213aebaf5b65b487a38
Brandon Madsen & Catherine Tran <student@epicodus.com>
2014-09-22 11:30:02 -0700

    create validation for presence and uniqueness of name and creates association between question and user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe User do
+  it { should validate_presence_of :user_name }
+  it { should validate_uniqueness_of :user_name }
+
+  it { should have_many :questions }
+end

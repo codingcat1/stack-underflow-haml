45157954ee18bdb4d1423eea6e6899250e8d6ba1
Catherine Tran <tranc489@gmail.com>
2014-09-26 16:46:09 -0700

    create ajax functionality for search method.  WORKS YAY.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,6 +8,10 @@ class QuestionsController < ApplicationController
     if params[:search].blank? == false
       @questions = Question.basic_search(params[:search])
     end
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def new
